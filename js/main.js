b209f3a3afe2439b90299c47140cd8e88ca38465
jashment <jordanashment@gmail.com>
2019-05-07 17:57:56 +0000

    Adding Sprites

--- js/main.js
+++ js/main.js
@@ -2,9 +2,17 @@ var game = new Phaser.Game(640, 360, Phaser.AUTO)
 var GameState = {
     preload: function() {
         this.load.image('background', 'assets/images/background.png')
+        this.load.image('chicken', 'assets/images/chicken.png')
+        this.load.image('horse', 'assets/images/horse.png')
+        this.load.image('pig', 'assets/images/pig.png')
+        this.load.image('sheep', 'assets/images/sheep.png')
     },
     create: function() {
         this.background = this.game.add.sprite(0, 0, 'background')
+
+        this.chicken = this.game.add.sprite(this.game.world.centerX, this.game.world.centerY, 'chicken')
+        this.chicken.anchor.setTo(0.5)
+        this.chicken.scale.setTo(1.5)
     },
     update: function() {
 
