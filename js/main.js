d707caabeda5f31e38d2ef9436bc805afa1ae282
jashment <jordanashment@gmail.com>
2019-05-07 17:48:08 +0000

    Adding Images

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 var game = new Phaser.Game(640, 360, Phaser.AUTO)
 var GameState = {
     preload: function() {
-
+        this.load.image('background', 'assets/images/background.png')
     },
     create: function() {
-
+        this.background = this.game.add.sprite(0, 0, 'background')
     },
     update: function() {
 
